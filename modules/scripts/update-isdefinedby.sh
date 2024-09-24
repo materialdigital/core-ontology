85b1f34c06d036952875c0cc0ba635116a6de2bb
joergwa <waitelonis@yovisto.com>
2024-09-24 15:56:24 +0200

    Update update-isdefinedby.sh

--- modules/scripts/update-isdefinedby.sh
+++ modules/scripts/update-isdefinedby.sh
@@ -11,21 +11,28 @@
 
 java -jar robot.jar remove --select imports --input ../pmdco-characterization.ttl verify  --queries ../../.github/workflow_resources/qc-queries/isdefinedby-missing.sparql  
 cat isdefinedby-missing.csv | tr -d '\r' |awk 'NR>1 {print "<" $1 "> rdfs:isDefinedBy <https://w3id.org/pmd/co/characterization> ."}'  >> ../pmdco-characterization.ttl
+rm isdefinedby-missing.csv
 
 java -jar robot.jar remove --select imports --input ../pmdco-datatransformation.ttl verify --queries ../../.github/workflow_resources/qc-queries/isdefinedby-missing.sparql  
 cat isdefinedby-missing.csv | tr -d '\r' |awk 'NR>1 {print "<" $1 "> rdfs:isDefinedBy <https://w3id.org/pmd/co/datatransformation> ."}'  >> ../pmdco-datatransformation.ttl
+rm isdefinedby-missing.csv
 
 java -jar robot.jar remove --select imports --input ../pmdco-devices.ttl  verify --queries ../../.github/workflow_resources/qc-queries/isdefinedby-missing.sparql  
 cat isdefinedby-missing.csv | tr -d '\r' |awk 'NR>1 {print "<" $1 "> rdfs:isDefinedBy <https://w3id.org/pmd/co/devices> ."}'  >> ../pmdco-devices.ttl
+rm isdefinedby-missing.csv
 
 java -jar robot.jar remove --select imports --input ../pmdco-logistics.ttl  verify --queries ../../.github/workflow_resources/qc-queries/isdefinedby-missing.sparql  
 cat isdefinedby-missing.csv | tr -d '\r' |awk 'NR>1 {print "<" $1 "> rdfs:isDefinedBy <https://w3id.org/pmd/co/logistics> ."}'  >> ../pmdco-logistics.ttl
+rm isdefinedby-missing.csv
 
 java -jar robot.jar remove --select imports --input ../pmdco-manufacturing.ttl verify --queries ../../.github/workflow_resources/qc-queries/isdefinedby-missing.sparql  
 cat isdefinedby-missing.csv | tr -d '\r' |awk 'NR>1 {print "<" $1 "> rdfs:isDefinedBy <https://w3id.org/pmd/co/manufacturing> ."}'  >> ../pmdco-manufacturing.ttl
+rm isdefinedby-missing.csv
 
 java -jar robot.jar remove --select imports  --input ../pmdco-materials.ttl verify --queries ../../.github/workflow_resources/qc-queries/isdefinedby-missing.sparql  
 cat isdefinedby-missing.csv | tr -d '\r' |awk 'NR>1 {print "<" $1 "> rdfs:isDefinedBy <https://w3id.org/pmd/co/materials> ."}'  >> ../pmdco-materials.ttl
+rm isdefinedby-missing.csv
 
 java -jar robot.jar remove --select imports  --input ../pmdco-qualities.ttl verify --queries ../../.github/workflow_resources/qc-queries/isdefinedby-missing.sparql  
 cat isdefinedby-missing.csv | tr -d '\r' |awk 'NR>1 {print "<" $1 "> rdfs:isDefinedBy <https://w3id.org/pmd/co/qualities> ."}'  >> ../pmdco-qualities.ttl
+rm isdefinedby-missing.csv
