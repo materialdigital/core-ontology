08b407e8dd3ee7a115301c7a9811f3ce75d33297
joergwa <waitelonis@yovisto.com>
2025-10-10 09:11:58 +0200

    update autoshapes, removed owl:real

--- src/ontology/utils/generate-auto-shapes.sh
+++ src/ontology/utils/generate-auto-shapes.sh
@@ -1,6 +1,6 @@
 
 # create the reasoned version of the edit ontology
-sh run.sh robot --catalog catalog-v001.xml merge --input pmdco-edit.owl reason --reasoner hermit --axiom-generators "SubClass SubObjectProperty ClassAssertion InverseObjectProperties PropertyAssertion" query --update utils/remove-isabout.sparql --output ./tmp/tmp-reasoned.ttl
+sh run.sh robot --catalog catalog-v001.xml merge --input pmdco-edit.owl remove --term owl:real reason --reasoner hermit --axiom-generators "SubClass SubObjectProperty ClassAssertion InverseObjectProperties PropertyAssertion" query --update utils/remove-isabout.sparql --output ./tmp/tmp-reasoned.ttl
 
 # generate the shacle shapes from the 
 docker run --rm -v ./:/data ghcr.io/ashleycaselli/shacl:latest infer -datafile /data/tmp/tmp-reasoned.ttl -shapesfile /data/utils/owl2shacl/owl2sh-open.ttl > ../../patterns/autoshape/auto-shapes-open.ttl
