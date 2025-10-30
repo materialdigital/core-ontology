1ef5317bdab62236a6988f92a714c6cc63dfc40b
joergwa <waitelonis@yovisto.com>
2025-10-30 15:16:54 +0100

    release preps

--- src/ontology/release.sh
+++ src/ontology/release.sh
@@ -1,5 +1,5 @@
-VERSION=3.0.0-alpha3
-PRIOR_VERSION=3.0.0-alpha2
+VERSION=3.0.0-alpha4
+PRIOR_VERSION=3.0.0-alpha3
 ONTBASE=https://w3id.org/pmd/co/
 ANNOTATE_ONTOLOGY_VERSION="annotate -V $ONTBASE$VERSION/\$@ --annotation owl:versionInfo $VERSION"
 
